5634dc61c99b64c9bcfdd01fe82ae989ae4d901f
C21381381 Eduard Dravnieks <c21381381@mytudublin.ie>
2025-12-09 02:15:11 +0000

    Code supposdely works but no audio plays even though wav is being recorded and read properly

--- vr-loopstation/scenes/audiocore.gd
+++ vr-loopstation/scenes/audiocore.gd
@@ -1,35 +1,34 @@
 extends MeshInstance3D
 
-@export var AudioBlock: AudioStreamPlayer3D
-@export var Play: AudioStreamPlayer3D
-
-var effect: AudioEffectRecord
+var effect: AudioEffect
 var recording: AudioStreamWAV
-var idx: int
 
-func _ready():
-	idx = AudioServer.get_bus_index(&"RecordBus")
+var stereo: bool = true
+var mix_rate := 44100  # This is the default mix rate on recordings.
+var format := AudioStreamWAV.FORMAT_16_BITS  # This is the default format on recordings.
+
+func _ready() -> void:
+	var idx := AudioServer.get_bus_index(&"RecordBus")
 	effect = AudioServer.get_bus_effect(idx, 0) as AudioEffectRecord
 
 
 func _on_area_3d_area_entered(_area):
-
-	# STOP RECORDING → PLAY
+	# STOP RECORDING
 	if effect.is_recording_active():
 		effect.set_recording_active(false)
 		print("Recording stopped.")
 
 		recording = effect.get_recording()
-
-		if recording:
-			# Set loop BEFORE playback
-			AudioBlock.stream = recording
-			AudioBlock.play()
-			print("Playing looped recording.")
+	else:
+		effect.set_recording_active(true)
+		print("Status Recording")
 		
-		return  
-
-	# START RECORDING
-	print("Recording...")
-	AudioBlock.stop()
-	effect.set_recording_active(true)
+		if recording:
+			print_rich("\n[b]Playing recording:[/b] %s" % recording)
+			print_rich("[b]Format:[/b] %s" % ("8-bit uncompressed" if recording.format == 0 else "16-bit uncompressed" if recording.format == 1 else "IMA ADPCM compressed"))
+			print_rich("[b]Stereo:[/b] %s" % ("Yes" if recording.stereo else "No"))
+			var data := recording.get_data()
+			print_rich("[b]Size:[/b] %s bytes" % data.size())
+			print("Playing looped recording.")
+			$Area3D/AudioStreamPlayer3D.stream = recording
+			$Area3D/AudioStreamPlayer3D.play()
