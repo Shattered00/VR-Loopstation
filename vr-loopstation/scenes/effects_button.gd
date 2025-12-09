661ae407c9e4ea6ab327ad9c299a17f2542154c4
C21381381 Eduard Dravnieks <c21381381@mytudublin.ie>
2025-12-09 09:46:37 +0000

    Color adjustments

--- vr-loopstation/scenes/effects_button.gd
+++ vr-loopstation/scenes/effects_button.gd
@@ -5,16 +5,6 @@ var effect_slot := 1  # Slot where your Reverb effect is located
 func _on_area_3d_area_entered(area):
 	var bus := AudioServer.get_bus_index(bus_name)
 
-	# CHECK 1: Does the bus exist?
-	if bus == -1:
-		push_error("Bus '%s' does NOT exist!" % bus_name)
-		return
-
-	# CHECK 2: Does an effect exist in this slot?
-	if effect_slot >= AudioServer.get_bus_effect_count(bus):
-		push_error("No effect found in slot %s on bus '%s'!" % [effect_slot, bus_name])
-		return
-
 	# Retrieve the current enabled state
 	var is_enabled := AudioServer.is_bus_effect_enabled(bus, effect_slot)
 
@@ -23,7 +13,9 @@ func _on_area_3d_area_entered(area):
 
 	# Print results
 	if not is_enabled:
+		$".".get_active_material(0).albedo_color = Color(0, 0, 1)
 		print(" Reverb ENABLED on %s slot %s" % [bus_name, effect_slot])
 	else:
-		print("✖ Reverb DISABLED on %s slot %s" % [bus_name, effect_slot])
+		$".".get_active_material(0).albedo_color = Color(128, 128, 128)
+		print(" Reverb DISABLED on %s slot %s" % [bus_name, effect_slot])
 	
