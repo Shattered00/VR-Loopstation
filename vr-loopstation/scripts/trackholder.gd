7ce2baa31d77b24753e0a8ce73843a334104d394
C21381381 Eduard Dravnieks <c21381381@mytudublin.ie>
2026-02-19 21:09:31 +0000

    More stuff done i guess. Scenes and updates

--- vr-loopstation/scripts/trackholder.gd
+++ vr-loopstation/scripts/trackholder.gd
@@ -5,12 +5,11 @@ signal interacted(control: Trackholder, hand: Node)
 
 @export var action_id: StringName
 
-@onready var touch_area: Area3D = $Area3D
+
 var _locked := false
 
 func _ready():
 	add_to_group(&"trackholder") 
-	touch_area.area_entered.connect(_on_area_entered)
 
 func _on_area_entered(area: Area3D):
 	if _locked:
